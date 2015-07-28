49e1811341b026a2cffa50070c0f2f4d09d17401
Xueping Long <gregllong@gmail.com>
2015-07-28 15:07:16 -0700

    moved analysis settings(config) file to config repo

--- multiplexerclient_config.py
+++ multiplexerclient_config.py
@@ -4,8 +4,7 @@ class multiplexer_config(object):
     info is the configuration dictionary in the form
     {channel_name: (port, hint, display_location, stretched)), }
     '''
-    info = {
-            'Repump'              : (1, '384.232000', (1,1), True),
+    info = {'Repump'              : (1, '384.232000', (1,1), True),
             'Pebbles - MOT light' : (3, '384.229085', (1,3), True),
             'Bamm-Bamm'           : (4, '384.228903', (5,1), True),
             'Ti:Sapph'            : (5, '385.284', (5,3), True),
